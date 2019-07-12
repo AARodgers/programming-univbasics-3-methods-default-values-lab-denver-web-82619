b33dda742bd26aec7c4e7dfeda70e7a91f5e50ab
Amanda Rodgers <aarodgerswork@gmail.com>
2019-07-12 00:09:07 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,4 +2,5 @@ def meal_choice(veg1, veg2, protein)
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   if protein = NIL
-    puts 
+    puts  "A plate of meat with #{veg1} and #{veg2}."
+end    
